43aa82edf8dc0d7ed0e11521e7ecc89585fa05d3
Pedro Lambert <pedrolambert@gmail.com>
2016-12-15 20:32:16 -0500

    Create `AppendEntry` wrapper for JSON

---
+++ AppendEntry.cpp
@@ -0,0 +1,58 @@
+#include "AppendEntry.h"
+#include "_Server.h"
+
+using namespace locke;
+
+template <size_t N>
+AppendEntry<N>::AppendEntry(StaticJsonBuffer<N>& json, const char* msg)
+{
+  _json = json.parseObject(msg);
+}
+
+template <size_t N>
+AppendEntry<N>::AppendEntry
+(StaticJsonBuffer<N>& json, Server& svr, LogEntry& entry)
+{
+  _json = json.createObject();
+
+  _json["term"] = svr.current_term;
+  _json["leader"] = svr.name;
+  _json["prev_index"] = entry.idx - 1;
+  _json["entry"] = entry.value;
+}
+
+template <size_t N>
+uint32_t AppendEntry<N>::term()
+{
+  return _json["term"];
+}
+
+template <size_t N>
+char AppendEntry<N>::leader()
+{
+  return _json["leader"][0];
+}
+
+template <size_t N>
+uint32_t AppendEntry<N>::prev_index()
+{
+  return _json["prev_index"];
+}
+
+template <size_t N>
+uint32_t AppendEntry<N>::leader_commit()
+{
+  return _json["leader_commit"];
+}
+
+template <size_t N>
+const char* AppendEntry<N>::entry()
+{
+  return _json["entry"];
+}
+
+template <size_t N>
+void AppendEntry<N>::print()
+{
+  _json.printTo(Serial);
+}
