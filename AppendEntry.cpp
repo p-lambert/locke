336c1d4abedf405bb4d2cb4dccf9ecea1759e0e6
Pedro Lambert <pedrolambert@gmail.com>
2016-12-17 14:24:53 -0500

    Use more compact JSON payload

--- AppendEntry.cpp
+++ AppendEntry.cpp
@@ -12,41 +12,41 @@ AppendEntry::AppendEntry
  const LogEntry& entry,
  const LogEntry& previous_entry) : _json(json.createObject())
 {
-  _json["term"] = server.current_term;
-  _json["leader"] = (uint8_t)server.name;
-  _json["prev_index"] = previous_entry.idx;
-  _json["prev_term"] = previous_entry.term;
-  _json["entry"] = entry.value;
+  _json[AE_TERM] = server.current_term;
+  _json[AE_LEADER] = (uint8_t)server.name;
+  _json[AE_PREV_IDX] = previous_entry.idx;
+  _json[AE_PREV_TERM] = previous_entry.term;
+  _json[AE_VALUE] = entry.value;
 }
 
 uint32_t AppendEntry::term() const
 {
-  return _json["term"];
+  return _json[AE_TERM];
 }
 
 char AppendEntry::leader() const
 {
-  return (uint8_t)_json["leader"];
+  return (uint8_t)_json[AE_LEADER];
 }
 
 uint32_t AppendEntry::prev_index() const
 {
-  return _json["prev_index"];
+  return _json[AE_PREV_IDX];
 }
 
 uint32_t AppendEntry::prev_term() const
 {
-  return _json["prev_term"];
+  return _json[AE_PREV_TERM];
 }
 
 const char* AppendEntry::entry() const
 {
-  return _json["entry"];
+  return _json[AE_VALUE];
 }
 
 bool AppendEntry::isHeartbeat() const
 {
-  return !_json.containsKey("entry") || strlen(_json["entry"]) == 0;
+  return !_json.containsKey(AE_VALUE) || strlen(_json[AE_VALUE]) == 0;
 }
 
 void AppendEntry::print()
--- Constants.hpp
+++ Constants.hpp
@@ -6,3 +6,10 @@
 #define CFG_FILE_NAME "svr"
 #define INITIAL_TERM 1
 #define NOBODY_YET '-'
+
+// AppendEntry payload keys
+#define AE_TERM "t"
+#define AE_LEADER "l"
+#define AE_PREV_IDX "pi"
+#define AE_PREV_TERM "pt"
+#define AE_VALUE "v"
